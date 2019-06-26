17512031874a697ff29c833afe45ab110dec0ac6
Leon <leon.id@qq.com>
2019-06-26 16:26:10 +0800

    功能增强：拦截窗口关闭，替换为隐藏；

--- DockingWinForms.ViaDockPanelSuite/Docks/DockForm2.cs
+++ DockingWinForms.ViaDockPanelSuite/Docks/DockForm2.cs
@@ -1,4 +1,5 @@
-﻿using WeifenLuo.WinFormsUI.Docking;
+﻿using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
 
 namespace DockingWinForms.ViaDockPanelSuite.Docks
 {
@@ -12,6 +13,25 @@ namespace DockingWinForms.ViaDockPanelSuite.Docks
             this.CloseButton = false;
             // 隐藏关闭按钮
             this.CloseButtonVisible = false;
+        }
+
+        /// <summary>
+        /// 窗口关闭
+        /// </summary>
+        /// <param name="e"></param>
+        /// <remarks>坑：this.Hide() 将引用到 DockContent().Hide()，请使用：((Form)this).Hide() 或 this.Visible = false</remarks>
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // 如果启用“隐藏窗口而非关闭”且用户主动请求关闭时
+            if (this.HideOnClose &&
+                e.CloseReason == CloseReason.UserClosing)
+            {
+                // 取消窗口关闭并手动隐藏窗口
+                e.Cancel = true;
+                this.Visible = false;
+            }
+
+            base.OnFormClosing(e);
         }
     }
 }
